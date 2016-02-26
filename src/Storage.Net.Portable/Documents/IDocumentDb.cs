fb8f7b31c2f08c02b743ff314b352b17df06ceff
Ivan Gavryliuk <ivan.gavrilyuk@gmail.com>
2016-02-26 10:30:40 +0000

    added first interface

---
+++ src/Storage.Net.Portable/Documents/IDocumentDb.cs
@@ -0,0 +1,29 @@
+﻿namespace Storage.Net.Documents
+{
+   /// <summary>
+   /// Interface for a simplest possible document DB
+   /// </summary>
+   public interface IDocumentDb
+   {
+      /// <summary>
+      /// Saves the document with specified ID
+      /// </summary>
+      void Save<T>(string id, T documentInstance);
+
+      /// <summary>
+      /// Loads a document by ID
+      /// </summary>
+      T Load<T>(string id);
+
+      /// <summary>
+      /// Checks if a document exists
+      /// </summary>
+      bool Exists<T>(string id);
+
+      /// <summary>
+      /// Deletes the document
+      /// </summary>
+      /// <returns>True if document was deleted, otherwise false.</returns>
+      bool Delete<T>(string id);
+   }
+}
